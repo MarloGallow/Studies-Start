14aeba685bcab9e5fd601d51a5cab12c9cdb0184
Marlo Gallow <mgallow226@noscihigh.org>
2021-02-10 20:58:38 +0000

    On string manipulation

--- index.js
+++ index.js
@@ -33,3 +33,131 @@ console.log(myName); // prints => bob
 var myVariable = 1;
 var myVariable = true;
 myVariable = "someString";
+
+/*
+*Data types
+*They can storage values. 
+/*
+*Numbers
+*Numbers are place Values/digits.
+*NUmbers start from 0.
+*You can use Numbers to add, subtract, multiply, and divide.
+//Example//
+myVar = 9 + 10;
+
+/*
+*String
+*They are characters surrounded in ("" or '').
+*They have acces to individual characters with bracket notation.
+*They can be combined with symbols like +.
+//Example
+myVar name = "KIllua Zoldyck";
+
+/*
+*Boolean
+*It gives a true or false values.
+*
+var name = Killua;
+name = Gon;
+return false:
+/
+/*Array
+*You start an array with brackets.
+*It can contain any data type.
+var myArray = ["Killua", "Luck", "Sasuke"];
+
+/*
+*Function
+*You can use functions to send a message.
+//Example
+function favFood() {
+  console.log("pizza");
+}
+/*
+*undefined
+* when a code is undefined it means that something doesn't have a value.
+//Example
+var d + 1;
+
+/*
+*NaN
+*You receive NaN when your value isn't a number.
+var pizza - 16; //receives not a number
+
+/*
+*Infinity and -infinity
+*Infinity represents positive infinity. -Infinity represents negative infinity.
+//Example
+var hugeNum = 2.99999999999937437387e+911,
+var hugeNum = 2.99999999999937437887e+912;
+console.log(hugeNum)
+console.log(hugeNum) //infinity
+
+/*
+*What is the difference between primitive/simple and complex data types?
+*Primitive data types are the simple data types.
+Simple data types are data types like strings, booleans, and objects.
+Complex data types that are made of other data types.
+//Example
+var myArray = ["Killua", "Luck", "Sasuke"];
+
+/*
+*Operators
+*They produce the value.
+//Example
+var = 9 + 10;
+/*
+*Assignment Operators
+*They give values to the variable
+//Example
+var = Taco;
+
+/*
+*Arithmatic Operators
+*They perform arithmatic on numbers.
+//Example
+var 3 * 11;
+
+/*
+*Comparison Operators
+*It compares two operators.
+//Example
+function hugeNum(myVal){
+  if (myVal ==99)
+  return "Equal";
+}
+
+/*
+*Logical Operators
+*Shows the logic between two nnumbers.
+//Example
+if (num < 90) {
+  if (num > 180) {
+    return "yes";
+  }
+}
+return "No";
+
+/*
+*String manipulation
+*It controls the string.
+//Example
+/*
+*With operators
+*
+//Example
+
+/*
+*with string method
+*
+//Example
+
+/*
+*Control flow
+*
+//Example
+
+/*
+*if
+*Using the keyword if to check for a specific condition.
+//Example
